a3dbda4526650278bb9991f3ef9afdb4adaef0e0
Lucas-Roseno <lucasroseno759@gmail.com>
2024-11-11 16:16:35 -0300

    Mudanças

---
+++ Laboratórios/lab2_java/src/lab2_java/Autor.java
@@ -0,0 +1,22 @@
+package lab2_java;
+
+public class Autor {
+	private String nome;
+	private String dataNascimento;
+	
+	//Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+}
---
+++ Laboratórios/lab2_java/src/lab2_java/Livro.java
@@ -0,0 +1,28 @@
+package lab2_java;
+
+public class Livro {
+	private String titulo;
+	private String ISBN;
+	private Autor autor = new Autor();
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public Autor getAutor() {
+		return autor;
+	}
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	
+	
+}
